6db0309495fe085b044395f8d9577274af94bf5b
G58 <freddy.fazbear.troll@gmail.com>
2023-11-13 14:21:34 -0600

    feat: Datos de todos los segmentos (#4)

--- segmentos.py
+++ segmentos.py
@@ -2,6 +2,9 @@ import xlsxwriter
 
 class Red_Segmentada:
 
+    # Información de cada segmento de red
+    data: list = []
+
     def __init__(self, file_name: str, sheet_name="segmentos") -> None:
         self.file_name = file_name
         self.sheet_name = sheet_name
@@ -10,3 +13,7 @@ class Red_Segmentada:
     def create_file(self) -> None:
         workbook = xlsxwriter.Workbook(f"{self.file_name}.xlsx")
         worksheet = workbook.add_worksheet(self.sheet_name)
+
+    # Recopilación de información de cada segmento
+    def add_segment(self, segment: list) -> None:
+        self.data.append(segment)
