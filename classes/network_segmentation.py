591848b0b662cff0c60f29ef1b0d122caa16a69f
G58 <freddy.fazbear.troll@gmail.com>
2023-11-19 00:01:53 -0600

    Métodos de clase e instancia

--- classes/network_segmentation.py
+++ classes/network_segmentation.py
@@ -10,7 +10,16 @@ class Segments:
     @classmethod
     def total_segments(cls) -> int:
         return cls.total_segments
-    
+
     @classmethod
     def add_segment(cls) -> None:
         cls.total_segments += 1
+
+    def next_segment(self) -> None:
+        pass
+
+    def host_bits(self) -> None:
+        pass
+
+    def get_network_info(self) -> None:
+        pass
