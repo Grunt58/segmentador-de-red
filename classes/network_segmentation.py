2c673fb3030225e1db74843f34e44c98bff49363
G58 <freddy.fazbear.troll@gmail.com>
2023-11-18 18:58:49 -0600

    build: class segments innit

--- classes/network_segmentation.py
+++ classes/network_segmentation.py
@@ -1,3 +1,15 @@
 import ipaddress
 
-print("Hello, world 😈")
+class segments:
+    total_segments: int = 0
+
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def total_segments(cls) -> int:
+        return cls.total_segments
+    
+    @classmethod
+    def add_segment(cls) -> None:
+        cls.total_segments += 1
