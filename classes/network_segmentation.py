6b60a2acf603e63b98d0ab44b55b05a26bac7cec
G58 <freddy.fazbear.troll@gmail.com>
2023-11-20 21:31:43 -0600

    refactor: cantidad de bits activos por segmento

--- classes/network_segmentation.py
+++ classes/network_segmentation.py
@@ -6,6 +6,7 @@ class Segments:
     def __init__(self, init_ip: str, hosts: list) -> None:
         self.init_ip = ipaddress.IPv4Address(init_ip)
         self.hosts = hosts
+        self.bits = []
 
     @classmethod
     def total_segments(cls) -> int:
@@ -18,8 +19,19 @@ class Segments:
     def next_segment(self) -> None:
         pass
 
-    def host_bits(self) -> None:
-        pass
+    def host_bits(self, mask: int) -> list:
+        # limpia los bits de la lista para el siguiente segmento
+        self.bits.clear()
+        # Cqntidad de bits activos por segmento
+        active_bits = 8 - (32 - mask)
+        # Agrege los bits activos '1' y los inactivos '0'
+        for _ in range(8):
+            if active_bits > 0:
+                self.bits.append(1)
+                active_bits -= 1
+            else:
+                self.bits.append(0)
+        return self.bits
 
     def get_network_info(self) -> None:
         pass
