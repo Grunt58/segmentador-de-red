9d125114be23799dc4adc6ea24520d5137d9793e
G58 <freddy.fazbear.troll@gmail.com>
2023-11-28 16:40:53 -0600

    feat!: Menú principal (#21)
    Se está mejorando la interfaz (consola) para que el usuario sea mas fácil elegir la opción que quiera y necesite, por ahora solo está la impresión en consola
    
    Close #5

--- main.py
+++ main.py
@@ -1,41 +1,45 @@
+import os
+
 from scripts import excel_tables
 from scripts import network_segmentation
+from resources import menu
+from resources import colors
 
-# Dirección base de la red
-RAW_direccion_base = str(input("Dirección base: "))
+# Clase menú
+main_menu: object = menu.Menu()
+# Para colorear la consola
+text: object = colors.TextColors
 
-# Solicitar al usuario la cantidad de hosts para cada red
-host_solicitados = [int(x) for x in input("Introduce la cantidad de hosts para cada red (separados por comas): ").split(",")]
-host_solicitados.sort(reverse=True)
+while True:
+    main_menu.show_menu()
+    user_input: int = main_menu.get_user_input()
 
-segmentos = network_segmentation.Segments(RAW_direccion_base, host_solicitados)
+    match user_input:
+        case 1:
+            network: str = str(input(f"{text.magenta}Dirección base: {text.reset}"))
+            hosts: list = [int(host) for host in input(f"{text.magenta}Cantidad de hosts (separados por comas): {text.reset}").split(",")]
 
-archivo = str(input("Nombre del archivo: "))
-tabla = excel_tables.Red_Segmentada(RAW_direccion_base, archivo)
-tabla.create_file()
+            new_segment: object = network_segmentation.Segments(network, hosts)
 
+            for host in hosts:
+                mask: int = new_segment.set_mask(host)
+                new_segment.host_bits(mask)
 
-for num_hosts in host_solicitados:
-    network_segmentation.Segments.add_segment()
-    # Calcular la máscara de subred necesaria
-    mascara = segmentos.set_mask(num_hosts)
+                new_segment.next_segment(mask)
 
-    # Guarda la cantidad de bits activos por segmento
-    segmentos.host_bits(mascara)
+                new_segment.add_segment()
+                curret_segment = f"SEGMENTO: {new_segment.get_total_segments()}"
+                print(f"{text.green}{curret_segment:-^40}{text.reset}")
+                new_segment.get_network_info()
 
-    # Genera la información del segmento actual
-    segmentos.next_segment(mascara)
+        case 2:
+            pass
 
-    # Muestra la información del segmento actual
-    curret_segment = f"SEGMENTO: {network_segmentation.Segments.get_total_segments()}"
-    print(f"{curret_segment:-^40}")
-    segmentos.get_network_info()    
+        case 3:
+            pass
 
-    # Exporta la información a Excel
+        case 4:
+            break
 
-    new_segment = segmentos.export_network_info()
-    new_segment.insert(0, network_segmentation.Segments.get_total_segments())
-    tabla.add_segment(new_segment)
-
-# El parámetro sirve para saber la longitud de la tabla
-tabla.create_table(network_segmentation.Segments.get_total_segments())
+        case _:
+            pass
