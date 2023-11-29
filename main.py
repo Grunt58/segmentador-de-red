ae9ee8ecae709938a5223fdda7edb3e88377698b
G58 <freddy.fazbear.troll@gmail.com>
2023-11-29 10:51:25 -0600

    feat: impresión en consola y Excel

--- main.py
+++ main.py
@@ -56,7 +56,31 @@ while True:
             table.create_table(new_segment.get_total_segments())
 
         case 3:
-            pass
+            network: str = str(input(f"{text.magenta}Dirección base: {text.reset}"))
+            hosts: list[int] = [int(host) for host in input(f"{text.magenta}Cantidad de hosts (separados por comas): {text.reset}").split(",")]
+            file_name: str = str(input(f"{text.magenta}Nombre de archivo: {text.reset}"))
+
+            new_segment: object = network_segmentation.Segments(network, hosts)
+            table: object = excel_tables.Red_Segmentada(network, file_name)
+            table.create_file()
+
+            for host in hosts:
+                new_segment.add_segment()
+
+                mask: int = new_segment.set_mask(host)
+                new_segment.host_bits(mask)
+
+                new_segment.next_segment(mask)
+
+                curret_segment = f"SEGMENTO: {new_segment.get_total_segments()}"
+                print(f"{text.green}{curret_segment:-^40}{text.reset}")
+                new_segment.get_network_info()
+
+                segment_to_table: list[str] = new_segment.export_network_info()
+                segment_to_table.insert(0, new_segment.get_total_segments())
+                table.add_segment(segment_to_table)
+
+            table.create_table(new_segment.get_total_segments())
 
         case 4:
             break
