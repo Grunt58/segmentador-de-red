de7029ad9c08f0ca928dab5622158423e5366400
G58 <freddy.fazbear.troll@gmail.com>
2023-11-30 01:44:56 -0600

    feat: Excel error handler
    La data para las tablas ahora son atributos de instancia, no de clase

--- scripts/excel_tables.py
+++ scripts/excel_tables.py
@@ -1,63 +1,80 @@
 import xlsxwriter
 
+from resources import colors
+
+text: object = colors.TextColors
+
 class Red_Segmentada:
 
     # Información de cada segmento de red
-    data: list = []
-    options = {
-    "data": data,
-    "columns": [
-        {
-            "header": "No.",
-        },
-        {
-            "header": "Host solicitados",
-        },
-        {
-            "header": "Host encontrados",
-        },
-        {
-            "header": "Dirección de red",
-        },
-        {
-            "header": "Máscara digital",
-        },
-        {
-            "header": "Máscara decimal",
-        },
-        {
-            "header": "Primera IP utilizable",
-        },
-        {
-            "header": "Última IP utilizable",
-        },
-        {
-            "header": "Dirección de BR",
-        }
-    ]
-}
+    
 
-    def __init__(self, base_IP: str, file_name: str, sheet_name="segmentos") -> None:
-        self.base_IP = base_IP
+    def __init__(self, file_name: str) -> None:
+        self.base_IP = None
         self.file_name = file_name
-        self.sheet_name = sheet_name
+        self.data: list = []
+        self.options = {
+        "data": self.data,
+        "columns": [
+            {
+                "header": "No.",
+            },
+            {
+                "header": "Host solicitados",
+            },
+            {
+                "header": "Host encontrados",
+            },
+            {
+                "header": "Dirección de red",
+            },
+            {
+                "header": "Máscara digital",
+            },
+            {
+                "header": "Máscara decimal",
+            },
+            {
+                "header": "Primera IP utilizable",
+            },
+            {
+                "header": "Última IP utilizable",
+            },
+            {
+                "header": "Dirección de BR",
+            }
+        ]
+    }
+    
+    def set_base_IP(self, base_IP: str) -> None:
+        self.base_IP = base_IP
 
     # Creamos el archivo de Excel (xlsx)
     def create_file(self) -> None:
         self.workbook = xlsxwriter.Workbook(f"{self.file_name}.xlsx")
-        self.worksheet = self.workbook.add_worksheet(self.sheet_name)
 
     # Recopilación de información de cada segmento
     def add_segment(self, segment: list) -> None:
         self.data.append(segment)
 
     # Creación de tabla con encabezados
-    def create_table(self, index) -> None:
+    def create_table(self, index: int, sheet_name: str) -> None:
+        self.worksheet = self.workbook.add_worksheet(sheet_name)
         self.worksheet.set_column("B:J", 20)
         self.worksheet.write("D2", "Dirección base")
         self.worksheet.write("E2", self.base_IP)
         self.worksheet.insert_image("B2", "bits.png")
         # 'index' sirve para saber la longitud de la tabla
         self.worksheet.add_table(f"B6:J{6 + index}", self.options)
-        # Creación del archivo
-        self.workbook.close()
+        self.data.clear()
+
+    def close_file(self) -> None:
+        # Al cerrar, se crea el archivo
+        while True:
+            try:
+                self.workbook.close()
+            except xlsxwriter.exceptions.FileCreateError as err:
+                decision = input(f"{text.red}[{err}]: Cierra el archivo antes de continuar...{text.reset}")
+                if decision != "n":
+                    continue
+            break
